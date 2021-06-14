454926fa326dd58d05f2e90b5c9dc8b833e9ad3c
Ernesto Ledesma <eledesma@proguidemc.com>
2021-06-14 18:12:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import Formulario from "./components/Formulario";
+import Header from "./components/Header";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Fragment>
+    
+     <Header
+     titulo='Clima react App'
+      />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario />
+            </div>
+          </div>
+        </div>
+    
+
+
+        <div className="col m6 s12">
+          2
+        </div>
+
+      </div>
+   </Fragment>
   );
 }
 
